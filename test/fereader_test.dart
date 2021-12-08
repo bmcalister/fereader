ae37de8dc0a134902dbfb723a540281bc21df4ae
Michal <winklermichu@gmail.com>
2021-12-08 16:00:43 +0100

    null safety migration

--- test/fereader_test.dart
+++ test/fereader_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:fereader/fereader.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('fereader');
